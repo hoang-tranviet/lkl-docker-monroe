5b5935e03949297ee3e8e4aed4ab6c90c09c3622
Hoang Tran <hoang.tran@uclouvain.be>
2017-11-16 16:29:05 +0100

    get per-iface gateway and default gateway info

--- files/main-script-json.sh
+++ files/main-script-json.sh
@@ -22,22 +22,35 @@ fi
 
 ip addr
 ip route
+ip rule
+ip route show table 10000
+ip route show table 10001
+
 
 ss -antop| grep 5556
 cat /etc/metadata-exporter.conf
 
 cd /opt/monroe
-./metadata
 
 # "Installing LKL package:"
 apt-get install -y ./*.deb
 
+./metadata
 
 LKL_HIJACK_CONFIG_FILE=$LKL_FILE   lkl-hijack \
  curl --resolve multipath-tcp.org:80:130.104.230.45 http://multipath-tcp.org
 
-LKL_HIJACK_CONFIG_FILE=$LKL_FILE   lkl-hijack \
- ./iperf3_profile --no-delay  -t 3 -c 130.104.230.97 -p 5209 -m $IF,$IF
+ifaces="$( cat lkl-config.json |grep param| cut -f4 -d '"')"
+ifcount="$(cat lkl-config.json |grep param| cut -f4 -d '"'| wc -l)"
+
+# if [[ $((ifcount)) == '2' ]]; then
+# 	IF1="$(echo ifaces| cut
+# 	LKL_HIJACK_CONFIG_FILE=$LKL_FILE   lkl-hijack \
+#  	./iperf3_profile  -Vd --no-delay -t 3 -c 139.162.73.214 -p 5209 -m $IF,$IF
+# else
+	LKL_HIJACK_CONFIG_FILE=$LKL_FILE   lkl-hijack \
+	./iperf3_profile  -Vd --no-delay -t 3 -c 139.162.73.214 -p 5209
+# fi
 
 
 #./metadata_subscriber.py
