d1d07de0ccc24613d9259fd89efa5947789977a9
Hoang Tran <hoang.tran@uclouvain.be>
2017-11-24 16:31:25 +0100

    add intermediate docker image lkl-base containing lkl

--- build.sh
+++ build.sh
@@ -4,5 +4,6 @@ echo $DIR
 CONTAINER=${DIR##*/}
 DOCKERFILE=${CONTAINER}.docker
 
-docker pull monroe/base
+# docker pull monroe/base
+docker pull tranviethoang/lkl-base
 docker build --rm --no-cache -f ${DOCKERFILE} -t ${CONTAINER} . && echo "Finished building ${CONTAINER}"
--- push.sh
+++ push.sh
@@ -4,6 +4,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 CONTAINER=${DIR##*/}
 
+# CONTAINERTAG=tranviethoang/lkl-base
 CONTAINERTAG=tranviethoang/lkl-test
 
 docker login && docker tag ${CONTAINER} ${CONTAINERTAG} && docker push ${CONTAINERTAG} && echo "Finished uploading ${CONTAINERTAG}"
