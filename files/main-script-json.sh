61df2dc6d55cf8da29cefe60a7905f6c58f804cb
Hoang Tran <hoang.tran@uclouvain.be>
2017-12-07 10:33:41 +0100

    auto switch to another server port
    if current server on port is busy or failed

--- files/main-script-json.sh
+++ files/main-script-json.sh
@@ -9,20 +9,19 @@ nodeid="$(cat /nodeid)"
 log_file=/monroe/results/output-${time}-${nodeid}.log
 
 # Check if we are on a managed monroe node
-nodeid="$(cat /nodeid)"   >> $log_file 2>&1
+"$(cat /nodeid)"   >> $log_file 2>&1
 
-if [ $? -eq 0 ]
-then
-  echo "On Monroe node"
+if [ $? -eq 0 ]; then
+  # echo "On Monroe node"
   cat /monroe/config
-  MONROE=true
+  # MONROE_NODE=true
   LKL=lkl-hijack
 # elif
 #   echo "On local node"
 #   LKL=lkl/bin/lkl-hijack.sh
 else
-  echo "On dev node"
-  DEVNODE=true
+  # echo "On dev node"
+  # DEVNODE=true
   LKL=lkl-hijack
   # need to create manually on dev node
   mkdir -p /monroe/results/
@@ -39,8 +38,6 @@ ip route show table 10001
 # cat /etc/metadata-exporter.conf
 
 cd /opt/monroe
-
-# apt-get update
 
 # "Installing LKL package:"
 apt-get install -y ./*.deb
@@ -62,59 +59,65 @@ echo "relevant ifaces: "$ifaces
 inlab_server="130.104.230.97"
 linode_server="139.162.73.214"
 
-port=$(( 5201 + RANDOM % 8))
-
-
 start_tcpdump() {
         server=$1
         trace="/monroe/results/dump-$test_id-$server-$nodeid-$2.pcap.log"
-        # trace="/monroe/results/tshark-$nodeid-$test_id.pcap"
-        echo "capturing packet trace to file: $trace"
+        # echo "capturing packet trace to file: $trace"
         # tcpdump  -i $2  -s 150 tcp and portrange 5201-5210  -w $trace  &
         tcpdump -i $2 -s 150 not icmp -w $trace  &
-        # tshark -w   $trace &
         sleep 0.3
 }
 
 kill_tcpdump() {
         echo "killing tcpdump"
-        sleep 2
         pkill tcpdump
         pkill tcpdump
-        # pkill tshark
 }
 
 iptables_bypass_kernel_stack() {
-	iptables -A OUTPUT -p tcp --tcp-flags RST RST -d $server -j DROP
+  # preventive measure: drop incoming pkt to stack
 	iptables -A INPUT -p tcp --source-port $port -j DROP	
+  # reactive measure: drop outgoing RST
+  iptables -A OUTPUT -p tcp --tcp-flags RST RST -d $server -j DROP
 }
+
 iptables_cleanup() {
-	iptables -D OUTPUT -p tcp --tcp-flags RST RST -d $server -j DROP
 	iptables -D INPUT -p tcp --source-port $port -j DROP
+  iptables -D OUTPUT -p tcp --tcp-flags RST RST -d $server -j DROP
 }
 
 for server in $inlab_server $linode_server; do
 	## Note: iperf binary won't work if renamed
-	iperf="./iperf3_profile -Vd --no-delay -t 14 -i 0 -c $server -p $port --test-id $test_id "
+	iperf="./iperf3_profile -Vd --no-delay -t 10 -i 0 -c $server --test-id $test_id "
 	cmd="LKL_HIJACK_CONFIG_FILE=$LKL_FILE   $LKL  $iperf"
 
 	IF1="$(echo $ifaces| cut -d' ' -f1)"
 
-	iptables_bypass_kernel_stack
 
-	start_tcpdump $server $IF1
+  start_tcpdump $server $IF1
 
-	if [[ $ifcount -gt 1 ]]; then
-	  IF2="$(echo $ifaces| cut -d' ' -f2)"
-	  # echo "$IF1 $IF2"
-	  cmd=$cmd" -m $IF1,$IF2"
-	  start_tcpdump $server $IF2
-	fi
+  if [[ $ifcount -gt 1 ]]; then
+    IF2="$(echo $ifaces| cut -d' ' -f2)"
+    # echo "$IF1 $IF2"
+    cmd=$cmd" -m $IF1,$IF2"
+    start_tcpdump $server $IF2
+  fi
 
-	(eval $cmd) >> $log_file 2>&1
+  error=1
 
-	iptables -L -v
-	iptables_cleanup
+  while [[ $error -eq 1 ]]; do
 
-	kill_tcpdump
+    port=$(( 5201 + RANDOM % 8))
+
+    iptables_bypass_kernel_stack
+
+    command=$cmd" -p $port"
+    # execute command here, output to log file
+    (eval $command) >> $log_file 2>&1
+    error=$?
+    # iptables -L -v
+    iptables_cleanup
+  done
+
+  kill_tcpdump
 done
