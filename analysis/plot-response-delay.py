1a9a1139441885ad4c8ca244fa633a9c795d3cf5
Hoang Tran <hoang.tran@uclouvain.be>
2018-01-29 17:36:56 +0100

    get gps speed and plot speed vs delay

--- analysis/plot-response-delay.py
+++ analysis/plot-response-delay.py
@@ -6,6 +6,7 @@
 # OUTPUT:
 # plot request-response delay of siri-like app
 
+from __future__ import print_function
 import os
 import pprint
 import argparse
@@ -23,6 +24,11 @@ parser = argparse.ArgumentParser(description="plot iperf test results from json"
 parser.add_argument('--expdir', '-d',
                     help="Directory which stores results (exp ID)",
                     default="15918/")
+parser.add_argument('--pdf', '-p',
+                    help="Save figures as PDF. By default we save as PNG only",
+                    action='store_true',
+                    default="False")
+
 
 args = parser.parse_args()
 
@@ -36,6 +42,24 @@ linode_ip ="139.162.73.214"
 delays = {"Inlab": {"TCP":[], "MPTCP":[]},
           "Japan": {"TCP":[], "MPTCP":[]}}
 
+
+def get_gps_speed(file):
+    speed = []
+    with open(file) as metadata:
+        for line in metadata:
+            try:
+                meta = json.loads(line)
+                if "Speed" in meta and meta["Speed"] != None:
+                    speed.append(meta["Speed"])
+            except ValueError:
+                pass
+        if speed:
+            avg_speed = sum(speed)/len(speed)
+            print ("speed: " + str(round(avg_speed,1)))
+            return avg_speed
+        else:
+            return None
+
 def get_SignalStrength(file):
     rssi = {}
     with open(file) as metadata:
@@ -46,10 +70,10 @@ def get_SignalStrength(file):
                     rssi[meta["InternalInterface"]] = meta["RSSI"]
             except ValueError:
                 pass
-        print rssi
+        print (rssi)
     return rssi
 
-def get_delays_from_iperf_output(file, rssi, primary_iface, type):
+def get_delays_from_iperf_output(file, rssi, speed, primary_iface, type):
 
     with open(file) as output_file:
         server = "Inlab"
@@ -65,23 +89,25 @@ def get_delays_from_iperf_output(file, rssi, primary_iface, type):
                 if pattern.search(line) != None:
                     str = pattern.search(line).group(0)
                     primary_iface = str.split(":")[1]
-                    print primary_iface
+                    print (primary_iface)
                     break
-
 
             if "Request-response delay:" in line:
                 delaystr = line.split(':')[1].strip()
-                # print("get delay: "+ delaystr)
+                print("delay: "+ delaystr+'\t', end="")
                 delay = float(delaystr)
 
                 signal = 0
                 if (primary_iface != None) and primary_iface in rssi:
                     signal = rssi[primary_iface]
                 else:
-                    print ("primary_iface not found")
+                    print (" primary_iface not found ")
+                    if not rssi:
+                        print (" RSSI not found ")
+                        return
                     signal = rssi[rssi.keys()[0]]
-
-                delays[server][type].append((delay, signal))
+                delays[server][type].append((delay, signal, speed))
+        print("")
 
 def get_primary_iface(file):
     iface = None
@@ -91,7 +117,7 @@ def get_primary_iface(file):
             if pattern.search(line) != None:
                 str = pattern.search(line).group(0)
                 iface = str.split("=")[1]
-                print iface
+                print (iface)
                 break
     return iface
 
@@ -109,6 +135,7 @@ def load_test_run_data():
         for file in os.listdir("./"):
             if file.startswith("metadata.log"):
                 rssi = get_SignalStrength(file)
+                speed = get_gps_speed(file)
             if file.startswith("container.log"):
                 primary_iface = get_primary_iface(file)
 
@@ -117,9 +144,11 @@ def load_test_run_data():
 
         for file in os.listdir("./"):
             if file.startswith("output-201"):
-                get_delays_from_iperf_output(file, rssi, primary_iface, type="MPTCP")
+                print("mptcp:")
+                get_delays_from_iperf_output(file, rssi, speed, primary_iface, type="MPTCP")
             if file.startswith("output-tcp"):
-                get_delays_from_iperf_output(file, rssi, primary_iface, type="TCP")
+                print("tcp:")
+                get_delays_from_iperf_output(file, rssi, speed, primary_iface, type="TCP")
 
         os.chdir("../..")
 
@@ -130,8 +159,9 @@ def load_test_run_data():
 
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.stats import linregress
 
-def plot_graph(datatype, server, legend='inside'):
+def plot_delay_vs_signal(datatype, server, legend='inside'):
     fig, ax = plt.subplots()
     # ax.set_facecolor(BACKGROUND_COLOR)
 
@@ -142,17 +172,47 @@ def plot_graph(datatype, server, legend='inside'):
         values = [v[0] for v in data]
         print(str(type) + " average of "+str(len(values)) +": "+ str(sum(values)/len(values)))
 
+        # get the correlation and p-value (confidence)
+        # print np.corrcoef(values, signals)[0, 1]
+        print(linregress(values, signals))
+
         ax.scatter(signals, values, label= type + " Delay")
 
     plt.legend(loc='best')
-    plt.xlabel('RSSI (dBm)')
+    plt.xlabel('RSSI of primary interface (dBm)')
     plt.ylabel(datatype + ' (' +'second'+ ')')
 
     plt.title(server + " server")
     plt.grid()
-    plt.show()
-    fig.savefig('Request-Response-Delay-'+ str(server)+'-Scatter.pdf', bbox_inches='tight')
+    # plt.show()
+    if (args.pdf == True):
+        fig.savefig('Request-Response-Delay-'+ str(server)+'-Scatter.pdf', bbox_inches='tight')
+    else:
+        fig.savefig('Request-Response-Delay-'+ str(server)+'-Scatter.png', bbox_inches='tight')
 
+def plot_delay_vs_speed(datatype, server, legend='inside'):
+    fig, ax = plt.subplots()
+
+    for type in ["TCP","MPTCP"]:
+        data = delays[server][type]
+        values = [v[0] for v in data]
+        speed = [v[2] for v in data]
+        if all(v is None for v in speed):
+            print("no gps speed info found")
+            return
+
+        ax.scatter(speed, values, label= type + " Delay")
+
+    plt.legend(loc='best')
+    plt.xlabel('Node speed (km/h)')
+    plt.ylabel(datatype + ' (' +'second'+ ')')
+
+    plt.title(server + " server")
+    plt.grid()
+    if (args.pdf == True):
+        fig.savefig('Delay-vs-Speed-'+ str(server)+'-Scatter.pdf', bbox_inches='tight')
+    else:
+        fig.savefig('Delay-vs-Speed-'+ str(server)+'-Scatter.png', bbox_inches='tight')
 
 def plot_cdf(datatype, server, legend='inside'):
     fig, ax = plt.subplots()
@@ -175,13 +235,17 @@ def plot_cdf(datatype, server, legend='inside'):
     plt.title(server + " server")
     plt.grid()
     # plt.show()
-    fig.savefig('Request-Response-Delay-'+ str(server)+'-CDF.pdf', bbox_inches='tight')
+    if (args.pdf == True):
+        fig.savefig('Request-Response-Delay-'+ str(server)+'-CDF.pdf', bbox_inches='tight')
+    else:
+        fig.savefig('Request-Response-Delay-'+ str(server)+'-CDF.png', bbox_inches='tight')
 
 load_test_run_data()
 os.chdir(exp_dir)
 
 for server in delays:
-    print delays[server],
+    # print delays[server],
     print("")
-    plot_graph('Request-Response Delay', server)
+    plot_delay_vs_signal('Request-Response Delay', server)
+    plot_delay_vs_speed('Request-Response Delay', server)
     plot_cdf('Request-Response Delay', server)
